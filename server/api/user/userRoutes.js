ec1fe2270fea329bc21a02bad30920d49568ee57
Ubuntu <ubuntu@ip-172-31-54-71.ec2.internal>
2016-12-16 21:33:08 +0000

    didnt have time to test this out. maybe put the error function into the original code that is there

--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -12,4 +12,22 @@ router.route('/')
     res.send({ok: true});
   });
 
+
+//hmm this currently does not work but I don't have time for the 5 points to sit and figure this out. If I had more time I could look into it.
+router.route('/api/users')
+        .get(function(err,req,res){
+           res.status(500);
+           res.render('500: there was an error',{error: err});
+        });
+
+//stand alone error handler from express docs
+function ErrorHandler(err,req,res,next){
+        if (res.headersSent){
+                return next(err);
+        }
+        res.status(500)
+        res.render('500: There was an error',{error: err})
+}
+
+
 module.exports = router;
